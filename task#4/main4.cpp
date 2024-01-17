63eedd8eedd2c8f693085c838caa97188a47daf0
zayts_home <10kcin01@gmail.com>
2024-01-18 02:16:49 +0500

    task#4 final

--- task#4/main4.cpp
+++ task#4/main4.cpp
@@ -11,14 +11,22 @@ void info_bills(int &bill_1h, int &bill_2h, int &bill_5h, int &bill_1k, int &bil
               << bill_1k << "\t1000\truble banknotes;\n"
               << bill_2k << "\t2000\truble banknotes;\n"
               << bill_5k << "\t5000\truble banknotes;\n"
-              << "Total amount:\t" << sum
-              << std::endl;
+              << "Total amount:\t" << sum << std::endl;
 }
-void read_file(int &bill_1h, int &bill_2h, int &bill_5h, int &bill_1k, int &bill_2k, int &bill_5k, int &sum)
+void read_file(int &bill_1h, int &bill_2h, int &bill_5h, int &bill_1k, int &bill_2k, int &bill_5k, int &sum, int &number_bills)
 {
+    number_bills = 0;
+    bill_1h = 0;
+    bill_1k = 0;
+    bill_2h = 0;
+    bill_2k = 0;
+    bill_5h = 0;
+    bill_5k = 0;
     std::ifstream bills("bills.bin", std::ios::binary);
+    bills.seekg(0);
     while (!bills.eof())
     {
+        number_bills++;
         int temp(0);
         bills.read((char *)&temp, sizeof(temp));
         sum += temp;
@@ -53,11 +61,14 @@ void read_file(int &bill_1h, int &bill_2h, int &bill_5h, int &bill_1k, int &bill
             continue;
         }
     }
+    number_bills--;
 }
-void plus()
+void plus(int &bill_1h, int &bill_2h, int &bill_5h, int &bill_1k, int &bill_2k, int &bill_5k, int &sum, int &number_bills)
 {
-    std::ofstream bills("bills.bin", std::ios::binary);
-    for (int i = 0; i < 10; i++)
+
+    std::srand(std::time(nullptr));
+    std::ofstream bills("bills.bin", std::ios::binary | std::ios::app);
+    for (int i = 0; i < 1000 - number_bills; i++)
     {
         int value[] = {100, 200, 500, 1000, 2000, 5000};
         bills.write((char *)&value[rand() % 6], sizeof(value[0]));
@@ -92,31 +103,13 @@ void saving_state(int &bill_1h, int &bill_2h, int &bill_5h, int &bill_1k, int &b
         bills.write((char *)&value[5], sizeof(value[0]));
     }
 }
-int function_min_bill(int &bill_1h, int &bill_2h, int &bill_5h, int &bill_1k, int &bill_2k, int &bill_5k)
+
+void minus(int &bill_1h, int &bill_2h, int &bill_5h, int &bill_1k, int &bill_2k, int &bill_5k, int &sum, int &number_bills)
 {
-    while (true)
-    {
-        if (bill_1h != 0)
-            return 100;
-        if (bill_2h != 0)
-            return 200;
-        if (bill_5h != 0)
-            return 500;
-        if (bill_1k != 0)
-            return 1000;
-        if (bill_2k != 0)
-            return 2000;
-        if (bill_5k != 0)
-            return 5000;
-    }
-}
-void minus(int &bill_1h, int &bill_2h, int &bill_5h, int &bill_1k, int &bill_2k, int &bill_5k, int &sum)
-{
-    int min_bill = function_min_bill(bill_1h, bill_2h, bill_5h, bill_1k, bill_2k, bill_5k);
     std::cout << "Enter how much money you want to withdraw: ";
     int money;
     std::cin >> money;
-    if (/*!(money % min_bill) &&*/ money <= sum) // money%200<bill_1h
+    if (money <= sum)
     {
         int temp_arr[6] = {0, 0, 0, 0, 0, 0};
         int temp_money = money;
@@ -155,16 +148,13 @@ void minus(int &bill_1h, int &bill_2h, int &bill_5h, int &bill_1k, int &bill_2k,
         if (money - temp_money == money)
         {
             sum -= money;
-            bill_5k -= temp_arr[0]; // temp_money / 5000 > bill_5k ? bill_5k : temp_money / 5000;
-            bill_2k -= temp_arr[1]; // temp_money / 2000 > bill_2k ? bill_2k : temp_money / 2000;
-            bill_1k -= temp_arr[2]; // temp_money / 1000 > bill_1k ? bill_1k : temp_money / 1000;
-            bill_5h -= temp_arr[3]; // temp_money / 500 > bill_5h ? bill_5h : temp_money / 500;
-            bill_2h -= temp_arr[4]; // temp_money / 200 > bill_2h ? bill_2h : temp_money / 200;
-            bill_1h -= temp_arr[5]; // temp_money / 100 > bill_1h ? bill_1h : temp_money / 100;
-
-            info_bills(bill_1h, bill_2h, bill_5h, bill_1k, bill_2k, bill_5k, sum);
-
-            saving_state(bill_1h, bill_2h, bill_5h, bill_1k, bill_2k, bill_5k);
+            bill_5k -= temp_arr[0];
+            bill_2k -= temp_arr[1];
+            bill_1k -= temp_arr[2];
+            bill_5h -= temp_arr[3];
+            bill_2h -= temp_arr[4];
+            bill_1h -= temp_arr[5];
+            number_bills -= (temp_arr[0] + temp_arr[1] + temp_arr[2] - temp_arr[3] + temp_arr[4] + temp_arr[5]);
         }
         else
             std::cout << "This operation is not possible: the requested amount cannot be\n withdrawn due to the lack of suitable banknotes in the machine" << std::endl;
@@ -174,24 +164,17 @@ void minus(int &bill_1h, int &bill_2h, int &bill_5h, int &bill_1k, int &bill_2k,
 }
 int main()
 {
-    std::srand(std::time(nullptr));
-    int bill_1h(0), bill_2h(0), bill_5h(0), bill_1k(0), bill_2k(0), bill_5k(0), sum(0);
+
+    std::string action;
+    int bill_1h(0), bill_2h(0), bill_5h(0), bill_1k(0), bill_2k(0), bill_5k(0), sum(0), number_bills(0);
     std::ifstream bills("bills.bin", std::ios::binary);
     if (bills.is_open())
     {
-        read_file(bill_1h, bill_2h, bill_5h, bill_1k, bill_2k, bill_5k, sum);
-        info_bills(bill_1h, bill_2h, bill_5h, bill_1k, bill_2k, bill_5k, sum);
-        std::string action;
-        std::cout << "Do you want to withdraw or deposit money into your account? ";
-        std::cin >> action;
-        if (action == "+")
-            plus();
-        if (action == "-")
-            minus(bill_1h, bill_2h, bill_5h, bill_1k, bill_2k, bill_5k, sum);
+        read_file(bill_1h, bill_2h, bill_5h, bill_1k, bill_2k, bill_5k, sum, number_bills);
     }
     else
     {
-        while (true)
+        while (action != "-1")
         {
             std::cout << "ATM empty" << std::endl;
             std::string action;
@@ -199,9 +182,28 @@ int main()
             std::cin >> action;
             if (action == "+")
             {
-                plus();
+                plus(bill_1h, bill_2h, bill_5h, bill_1k, bill_2k, bill_5k, sum, number_bills);
                 break;
             }
         }
+        read_file(bill_1h, bill_2h, bill_5h, bill_1k, bill_2k, bill_5k, sum, number_bills);
     }
+    while (action != "-1")
+    {
+        info_bills(bill_1h, bill_2h, bill_5h, bill_1k, bill_2k, bill_5k, sum);
+        std::cout << "Do you want to withdraw or deposit money into your account? ";
+        std::cin >> action;
+        if (action == "+")
+        {
+            plus(bill_1h, bill_2h, bill_5h, bill_1k, bill_2k, bill_5k, sum, number_bills);
+            read_file(bill_1h, bill_2h, bill_5h, bill_1k, bill_2k, bill_5k, sum, number_bills);
+        }
+        if (action == "-")
+        {
+            minus(bill_1h, bill_2h, bill_5h, bill_1k, bill_2k, bill_5k, sum, number_bills);
+            saving_state(bill_1h, bill_2h, bill_5h, bill_1k, bill_2k, bill_5k);
+            read_file(bill_1h, bill_2h, bill_5h, bill_1k, bill_2k, bill_5k, sum, number_bills);
+        }
+    }
+    saving_state(bill_1h, bill_2h, bill_5h, bill_1k, bill_2k, bill_5k);
 }
